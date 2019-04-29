cf64b204e2818f37c0807aa75e39933be72f5302
Yusuke Sangenya <longinus.eva@gmail.com>
2019-04-29 12:50:49 +0900

    auth

--- main.rb
+++ main.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 require 'toml-rb'
 require 'rack/contrib'
+require 'dotenv/load'
 
 FEEDS_TOML_PATH = './feeds.toml'.freeze
 
 use Rack::PostBodyContentTypeParser
+
+un = ENV.fetch("USERNAME")
+pw = ENV.fetch("PASSWORD")
+use Rack::Auth::Basic do |username, password|
+  username == un && password == pw
+end
 
 get '/' do
   File.read('./front/dist/index.html')
