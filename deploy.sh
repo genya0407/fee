b51efe3ad7e9e6ae6a25d3e0c7752f3a126abe79
Yusuke Sangenya <longinus.eva@gmail.com>
2019-04-29 13:03:44 +0900

    timer

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,9 @@ elm make src/Main.elm --output dist/index.html
 cd ..
 
 sudo cp service/fee.service /etc/systemd/system
+sudo cp service/fee-sendmail.service /etc/systemd/system
+sudo cp service/fee-sendmail.timer /etc/systemd/system
 sudo systemctl daemon-reload
+sudo systemctl enable fee.service
 sudo systemctl restart fee.service
+sudo systemctl enable fee-sendmail.timer
